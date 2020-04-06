a85bf0ce53a4042d898800ae040cad4097e86dd9
sahil5201 <62256341+sahil5201@users.noreply.github.com>
2020-04-06 16:18:55 +0530

    add bodyParser

--- src/api/routes/task.js
+++ src/api/routes/task.js
@@ -8,6 +8,7 @@ router.get('/',(req,res,next) => {
 })
 
 router.post('/',(req,res,next) => {
+    console.log(req.body)
     res.status(200).json({
         message:"it's POST"
     })
--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const admin = require("firebase-admin");
+var bodyParser = require('body-parser')
 
 //route
 const TaskRoute = require("./api/routes/task")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use("/Task",TaskRoute);
 
