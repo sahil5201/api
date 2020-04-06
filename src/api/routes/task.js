358ac716db05e7025589dc57b32c3e1c90638979
sahil5201 <62256341+sahil5201@users.noreply.github.com>
2020-04-06 15:07:59 +0530

    route commit

---
+++ src/api/routes/task.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+    res.status(200).json({
+        message:"it's GET"
+    })
+})
+
+router.post('/',(req,res,next) => {
+    res.status(200).json({
+        message:"it's POST"
+    })
+})
+
+
+module.exports = router
